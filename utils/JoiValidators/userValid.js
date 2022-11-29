130ac1eae0d5902b6ecaf1c0c0697f9499f78239
Dmitry Kovalev <doke174@yandex.ru>
2022-11-29 13:48:44 +0500

    fix: Joi user validation

--- utils/JoiValidators/userValid.js
+++ utils/JoiValidators/userValid.js
@@ -1,14 +1,4 @@
 const { celebrate, Joi } = require('celebrate');
-const validator = require('validator');
-const BadRequestError = require('../../errors/BadRequestErr');
-const { errMessages } = require('../constants');
-
-const urlValid = (url) => {
-  if (validator.isURL(url)) {
-    return url;
-  }
-  throw new BadRequestError(errMessages.incorrectUrl);
-};
 
 const validateLogin = celebrate({
   body: Joi.object().keys({
@@ -25,12 +15,6 @@ const validateRegister = celebrate({
   }),
 });
 
-const validateParamId = celebrate({
-  params: Joi.object().keys({
-    _id: Joi.string().hex().length(24),
-  }),
-});
-
 const validateUpdateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
@@ -38,26 +22,8 @@ const validateUpdateUser = celebrate({
   }),
 });
 
-const validateSaveMovie = celebrate({
-  body: Joi.object().keys({
-    country: Joi.string().required(),
-    director: Joi.string().required(),
-    duration: Joi.number().required(),
-    year: Joi.string().required(),
-    description: Joi.string().required(),
-    image: Joi.string().required().custom(urlValid),
-    trailerLink: Joi.string().required().custom(urlValid),
-    thumbnail: Joi.string().required().custom(urlValid),
-    movieId: Joi.number().required(),
-    nameRU: Joi.string().required(),
-    nameEN: Joi.string().required(),
-  }),
-});
-
 module.exports = {
   validateLogin,
   validateRegister,
-  validateParamId,
   validateUpdateUser,
-  validateSaveMovie,
 };
